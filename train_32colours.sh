3ec389424c4948a80bc758ec00f16786922be2d1
sushenghan <su2564468850@sjtu.edu.cn>
2022-08-29 14:47:58 +0800

    update

---
+++ train_32colours.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+CUDA_id=6
+
+CUDA_VISIBLE_DEVICES=${CUDA_id} python train.py -d cifar10 --num_colors 32
+CUDA_VISIBLE_DEVICES=${CUDA_id} python train.py -d cifar100 --num_colors 32
+CUDA_VISIBLE_DEVICES=${CUDA_id} python train.py -d stl10 --num_colors 32
+CUDA_VISIBLE_DEVICES=${CUDA_id} python train.py -d tiny200 --num_colors 32
